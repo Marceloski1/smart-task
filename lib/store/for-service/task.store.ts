bb9ce6d681aef22f69e7602bc20018ca72955024
Marceloski1 <pendragonn89@gmail.com>
2025-11-23 09:42:07 -0500

    Incorporate 3 stores for axios services in my client

---
+++ lib/store/for-service/task.store.ts
@@ -0,0 +1,89 @@
+"use client";
+
+import { create } from "zustand";
+import { Task , TaskCreate } from "@/lib/types";
+import { TaskService } from "@/service/task.service";
+
+interface TaskState {
+  tasks: Task[];
+  loading: boolean;
+  error: string | null;
+
+  // Actions
+  fetchTasks: (params?: { skip?: number; limit?: number; status?: string }) => Promise<void>;
+  getTask: (id: string) => Promise<Task | null>;
+  createTask: (data: TaskCreate) => Promise<Task | null>;
+  updateTask: (id: string, data: Partial<TaskCreate>) => Promise<Task | null>;
+  deleteTask: (id: string) => Promise<boolean>;
+}
+
+export const useTaskStore = create<TaskState>((set, get) => ({
+  tasks: [],
+  loading: false,
+  error: null,
+
+  fetchTasks: async (params = {}) => {
+    set({ loading: true, error: null });
+    try {
+      const tasks = await TaskService.fetchTasks()
+      set({ tasks });
+    } catch (err: any) {
+      console.error(err);
+      set({ error: err.message || "Error loading tasks" });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  getTask: async (id: string) => {
+    try {
+      return await TaskService.getById(id)
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  },
+
+  createTask: async (data: TaskCreate) => {
+    try {
+      const newTask = await TaskService.create(data);
+
+      // Añadir al store
+      set({ tasks: [...get().tasks, newTask] });
+      return newTask;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  },
+
+  updateTask: async (id: string, data: Partial<TaskCreate>) => {
+    try {
+      const updated = await TaskService.update(id, data);
+
+      set({
+        tasks: get().tasks.map((t) => (t.id === id ? updated : t)),
+      });
+
+      return updated;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  },
+
+  deleteTask: async (id: string) => {
+    try {
+      await TaskService.delete(id);
+
+      set({
+        tasks: get().tasks.filter((task) => task.id !== id),
+      });
+
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  },
+}));
